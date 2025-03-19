68f847ee58e7744df8cef4ae48272cdfd31e79c6
Prabhu Selvamani <prabhuselvamani1995@gmail.com>
2025-03-19 11:34:18 +0530

    Nativewind configured

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "./global.css"
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-primary text-3xl">Welcome!</Text>
+    </View>
+  );
+}
